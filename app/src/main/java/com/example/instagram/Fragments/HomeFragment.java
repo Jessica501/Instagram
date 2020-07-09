8d115828373972232d4f84743ad7e0b2945bf745
Jessica Xu <jyxu@fb.com>
2020-07-09 18:15:28 -0400

    Add fragments for home, compose and fragment and add functionality to switch between them

---
+++ app/src/main/java/com/example/instagram/Fragments/HomeFragment.java
@@ -0,0 +1,104 @@
+package com.example.instagram.Fragments;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instagram.PostsAdapter;
+import com.example.instagram.R;
+import com.example.instagram.databinding.FragmentHomeBinding;
+import com.example.instagram.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding;
+
+    public static final String TAG = "HomeFragment";
+
+    protected List<Post> allPosts;
+    protected PostsAdapter adapter;
+
+     public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        binding.rvPosts.setLayoutManager(layoutManager);
+        binding.rvPosts.setAdapter(adapter);
+        queryPosts();
+
+        binding.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryPosts();
+                binding.swipeContainer.setRefreshing(false);
+            }
+        });
+
+        binding.swipeContainer.setColorSchemeColors(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+
+
+
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.include(Post.KEY_DESCRIPTION);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post: posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername() + ", description: " + post.getDescription());
+                }
+                adapter.clear();
+                adapter.addAll(posts);
+            }
+        });
+
+    }
+}
